a423be15358856ed4f1362c669d3686e538d6c6a
Zaki Asmat <zaki.asmat@sheridancollege.ca>
2025-09-25 08:31:11 -0400

    revision 1

---
+++ src/main/java/git/practicegit/PracticeGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package git.practicegit;
+
+/**
+ *
+ * @author Zaki Asmat
+ */
+public class PracticeGit
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Code version 1");
+    }
+}
